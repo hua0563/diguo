f4ca4bc8fa5c3213a2aa580e03065d6f15ec7d73
qi360 <47762007@qq.com>
2017-03-21 12:07:56 +0800

    测试一
    测试王

--- index.php
+++ index.php
@@ -9,10 +9,6 @@ $link=db_connect();
 $empire=new mysqlquery();
 $pr=$empire->fetch1("select sitekey,siteintro from {$dbtbpre}enewspublic limit 1");
 //页面
-$pagetitle=ehtmlspecialchars($public_r['sitename']);
-$pagekey=ehtmlspecialchars($pr['sitekey']);
-$pagedes=ehtmlspecialchars($pr['siteintro']);
-$url="<a href=\"".ReturnSiteIndexUrl()."\">".$fun_r['index']."</a>";//栏目导航
 $indextemp=GetIndextemp();//取得模板
 $string=DtNewsBq('indexpage',$indextemp,0);
 $string=str_replace('[!--newsnav--]',$url,$string);//位置导航
